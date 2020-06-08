b5a8e740c5e4303e3e75bb686ffc49ea6636a797
Kaelin Alexander <kaelin.alexander@gmail.com>
2020-06-08 17:54:26 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -2,18 +2,18 @@ class CashRegister
 attr_accessor :total, :items
 @items = {}
 
-def initialize
-  @total = 0
-  discount = self.new - self.new*0.2
-end
-
-def total
-prices = []
-  items.collect.each do |item, price|
-  prices << price
+  def initialize
+    @total = 0
+    discount = self.new - self.new*0.2
   end
-  prices.inject(0) {|sum, i|  sum + i }
-end
+
+  def total
+  prices = []
+    items.collect.each do |item, price|
+    prices << price
+    end
+    prices.inject(0) {|sum, i|  sum + i }
+  end
     
 end
 
